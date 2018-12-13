1955d74c058b757f1b0cb92f51f172205b903e27
donovanky <donovanky@my.easternct.edu>
2018-12-13 15:26:34 -0500

    Rewind/Next Buttons not appearing
    The rewind/next button on the player bar work but no image appears.

---
+++ src/components/PlayerBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class PlayerBar extends Component {
+  render() {
+    return (
+      <section className="player-bar">
+        <section id="buttons">
+          <button id="previous" onClick={this.props.handlePrevClick}>
+            <span className="icon ion-skip-backward"></span>
+          </button>
+          <button id="play-pause" onClick={this.props.handleSongClick} >
+            <span className="icon ion-md-play"></span>
+            <span className="icon ion-md-pause"></span>
+          </button>
+          <button id="next" onClick={this.props.handleNextClick}>
+            <span className="icon ion-skip-forward"></span>
+          </button>
+        </section>
+        <section id="time-control">
+          <div className="current-time">–:––</div>
+          <input type="range" className="seek-bar" value="0" />
+          <div className="total-time">–:––</div>
+        </section>
+        <section id="volume-control">
+          <div className="icon ion-volume-low"></div>
+          <input type="range" className="seek-bar" value="80" />
+          <div className="icon ion-volume-high"></div>
+        </section>
+      </section>
+    );
+  }
+}
+export default PlayerBar;
