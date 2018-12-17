0e24070e2dc71c9613a7399b413bc1b5f0fd6d8d
donovanky <donovanky@my.easternct.edu>
2018-12-17 15:52:36 -0500

    Time displays minutes now

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -17,7 +17,7 @@ class PlayerBar extends Component {
           </button>
         </section>
         <section id="time-control">
-          <div className="current-time">{this.props.currentTime}</div>
+          <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
           <input
             type="range"
             className="seek-bar"
